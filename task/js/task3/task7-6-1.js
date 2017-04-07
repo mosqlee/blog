e2a61f81b0780e7b22925cf504a63e439ff591b5
mosqlee <liweiwen92@163.com>
2017-04-07 23:31:47 +0800

    JS任务四

--- task/js/task3/task7-6-1.js
+++ task/js/task3/task7-6-1.js
@@ -7,6 +7,7 @@ var speach = document.getElementById("speaches");
 var vote = document.getElementById("voter");
 var btn = document.getElementById("dropdownMenu1");
 var btns = document.getElementsByTagName("li");
+var Step2;
 //实现点击展开，再点收起的有限状态机
 function Button() {
 
@@ -15,9 +16,9 @@ Button.prototype = {
     //当前状态
     currentState : "open",
 //    绑定事件
-    initialize : function(a) {
-        this.transition(a);
-    },
+//     initialize : function(a) {
+//         this.transition(a);
+//     },
     transition : function(a) {
         switch(this.currentState) {
             case "open":
@@ -34,7 +35,7 @@ Button.prototype = {
     }
 };
 //调用状态机，将第一天设为可以关闭和开启
-var button1 =new Button();
+var button1 = new Button();
 btn.onclick = function() {
     button1.initialize(this)
 };
@@ -42,32 +43,67 @@ btn.onclick = function() {
 // 四个都点击过就跳转界面，点击第一个之后只能点下一个，点击其他的会弹提示
 //
 function Step() {
-
 }
 Step.prototype = {
     //当前状态，杀人步骤
-    currentState: "killer",
+    currentState: "1",
 //    绑定事件，加一些函数
-    initialize: function(a) {
+//     initialize: function(a) {
+//         this.transition(a);
+//     },
+    //循环改变点击事件
+    change_onclick: function (a,Steps) {
         var m;
         var object1 = {};
-        this.transition(a);
-        //抓取父元素的元素节点
-        for (var x = 0,i = 0;x < a.parentNode.childNodes.length;x++) {
+        var count = 0;
+        var n;
+//抓取父元素的元素节点
+        for (var x = 0, i = 0; x < a.parentNode.childNodes.length; x++) {
             if (a.parentNode.childNodes[x].nodeType == 1) {
                 object1[i] = a.parentNode.childNodes[x];
                 i++;
             }
         }
         console.log(object1);
-        //抓取a在父元素的子元素中的第几个
-        for (var y = 0;y < btns.length;y++) {
-            if (btns[y].innerHTML == a.innerHTML) {
-                m = y;
-                console.log(m);
+        for (i in object1) {
+            count++;
+        }
+        for (var i = 0; i < count; i++) {
+            if (object1[i].innerHTML == a.innerHTML) {
+                m = i;
             }
         }
     },
+    //     for (var y = 0;y < count;y++) {
+    //         //节点前的确认改为“请进行下一项”
+    //         if (y < m + 1) {
+    //             object1[y].onclick = function () {
+    //                 confirm('请进行下一项活动');
+    //             };
+    //         }
+    //         else if (y == m + 1 && y == 2) {
+    //             object1[y].onclick = function () {
+    //                 alert("请死者亮明身份并且发表遗言");
+    //                 Steps.transition(this);
+    //             }
+    //         }
+    //         else if (y == m + 1 && y == 3) {
+    //             object1[y].onclick = function (Steps) {
+    //                 alert("玩家依次发言");
+    //                 Steps.color(this);
+    //                 Steps.transition(this);
+    //             }
+    //         }
+    //         else if (y == m + 1 && y == 4) {
+    //             object1[y].onclick = function (Steps) {
+    //                 Steps.color(this);
+    //                 Steps.transition(this);
+    //             }
+    //         }
+    //     }
+    //     a = btns[m+1];
+    // },
+
     //改变背景色函数
     color: function(a) {
         a.style.background = "#83b09a";
@@ -81,28 +117,36 @@ Step.prototype = {
 //    转换状态
     transition: function(a) {
         switch (this.currentState) {
-            case "killer":
-                this.currentState = "dead";
+            case "1":
+                console.log(this.currentState);
+                this.currentState = "2";
                 this.color(a);
-                this.pls_next(a);
+                this.change_onclick(a,Steps);
+                console.log(this.currentState);
                 break;
-            case "dead":
-                this.currentState = "speach";
-
+            case "2":
+                console.log(this.currentState);
+                this.currentState = "3";
+                this.color(a);
+                this.change_onclick(a,Steps);
+                console.log(this.currentState);
                 break;
-            case "speach":
-                this.currentState = "vote";
-
+            case "3":
+                this.currentState = "4";
+                this.color(a);
+                this.change_onclick(a,Steps);
                 break;
-            case "vote":
-                this.currentState = "vote";
+            case "4":
+                this.currentState = "4";
+                this.color(a);
+                var Step2 = new Step();
                 break;
         }
 }
 };
-var Step1 =new Step();
+var Step1 =new Step(1);
 killer.onclick = function() {
-    Step1.initialize(this)
+    Step1.transition(this,Step1)
 };
 //初始状态其他无法点击
 for (var y = 1;y < btns.length;y++) {
---
+++ task/js/task3/task7-6-3.js
@@ -0,0 +1,126 @@
+/**
+ * Created by Administrator on 2017/4/7.
+ */
+/**
+ * Created by Mosqlee on 2017/4/7.
+ */
+var storage = window.sessionStorage;
+var s = storage.data;
+var Step1 = {};
+if (s) {
+    Step1 = JSON.parse(s);
+}
+else {
+    Step1 =new Step();
+}
+console.log(Step1);
+var killer = document.getElementById("kill");
+var dead = document.getElementById("death");
+var speach = document.getElementById("speaches");
+var vote = document.getElementById("voter");
+var btn = document.getElementById("dropdownMenu1");
+var btns =document.getElementsByTagName("li");
+
+var state1, state2;
+//定义对象
+function Step() {
+}
+Step.prototype = {
+//    当前状态，步骤1
+    state1: "1",
+    //切换步骤状态机
+    trans1: function() {
+        switch (this.state1) {
+            case "1":
+                this.state1="2";
+                change_click();
+                break;
+            case "2":
+                this.state1="3";
+                change_click();
+                break;
+            case "3":
+                this.state1="4";
+                change_click();
+                break;
+            default:
+                console.log("Invalid State");
+                break;
+        }
+    },
+    //切换开启关闭状态机
+    state2: "1",
+    trans2: function(a) {
+        switch(this.state2) {
+
+            case "1":
+                this.state2 = "2";
+                a.parentNode.className = "dropdown";
+                break;
+            case "2":
+                this.state2 = "1";
+                a.parentNode.className = "open dropdown";
+                break;
+            default:
+                console.log("Invalid State")
+        }
+    }
+};
+//循环写法
+
+function change_click() {
+    for (var i = 0; i < btns.length; i++) {
+        var num = parseInt(Step1.state1);
+        if (i < num - 1) {
+            c_color(btns[i]);
+            btns[i].onclick = function () {
+                confirm("请进行下一项活动");
+            }
+        }
+        else if (i == num - 1) {
+            btns[i].onclick = function () {
+                Step1.trans1();
+                c_color(this);
+                if (num == 1) {
+                    var storage = window.sessionStorage;
+                    var s = JSON.stringify(Step1);
+                    storage.setItem("data", s);
+                    console.log(storage.data);
+                    window.location.href="task7-7.html";
+                }
+                else if (num == 2) {
+                    confirm("请死者亮明身份并且发表遗言")
+                }
+                else if (num == 3) {
+                    confirm("玩家依次发言");
+                }
+                else if (num == 4) {
+                    var s = JSON.stringify(Step1);
+                    storage.setItem("data", s);
+                    console.log(storage.data);
+                    window.location.href="task7-8.html";
+
+                }
+            }
+        }
+        else if (i > num - 1) {
+            btns[i].onclick = function () {
+                confirm("请按顺序执行");
+            }
+        }
+    }
+}
+document.getElementsByTagName("body")[0].onload = function() {
+    change_click();
+};
+//改变背景色
+function c_color(a) {
+    a.style.background = "#83b09a";
+    a.childNodes[0].style.borderColor =
+        "transparent #83b09a transparent transparent";
+}
+//调用状态机将第一天设为可以关闭和开启
+btn.onclick = function() {
+    Step1.trans2(this);
+};
+
